e3c16287f43aeb16612126484081d633edab437e
kylegnally <kylegnally@gmail.com>
2020-05-26 10:50:06 -0400

    Refactor names in main plugin file. MANUALLY, because Resharper doesn't work for php in Visual Studio.
    The fact that JetBrains makes and owns PHPStorm, a "better" php IDE than VS, couldn't possibly be relevant.

--- miiie-module-import/miiie-module-import.php
+++ miiie-module-import/miiie-module-import.php
@@ -10,18 +10,18 @@
  *
  * @link              http://example.com
  * @since             1.0.0
- * @package           Plugin_Name
+ * @package           MIIIE_Module_Import
  *
  * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
+ * Plugin Name:       MIIIE Module Import
  * Plugin URI:        http://example.com/plugin-name-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Description:       Plugin to import curriculum modules into a custom Module post type containing custom fields.
  * Version:           1.0.0
- * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
+ * Author:            Kyle G. Nally
+ * Author URI:        https://github.com/kylegnally
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       plugin-name
+ * Text Domain:       miiie-module-import
  * Domain Path:       /languages
  */
 
@@ -41,28 +41,28 @@ define( 'PLUGIN_NAME_VERSION', '1.0.0' );
  * The code that runs during plugin activation.
  * This action is documented in includes/class-plugin-name-activator.php
  */
-function activate_plugin_name() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-activator.php';
-	Plugin_Name_Activator::activate();
+function activate_miiie-module-import() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-miiie-module-import-activator.php';
+	MIIIE_Module_Import_Activator::activate();
 }
 
 /**
  * The code that runs during plugin deactivation.
  * This action is documented in includes/class-plugin-name-deactivator.php
  */
-function deactivate_plugin_name() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-deactivator.php';
-	Plugin_Name_Deactivator::deactivate();
+function deactivate_miiie-module-import() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-miiie-module-import-deactivator.php';
+	MIIIE_Module_Import_Deactivator::deactivate();
 }
 
-register_activation_hook( __FILE__, 'activate_plugin_name' );
-register_deactivation_hook( __FILE__, 'deactivate_plugin_name' );
+register_activation_hook( __FILE__, 'activate_miiie-module-import' );
+register_deactivation_hook( __FILE__, 'deactivate_miiie-module-import' );
 
 /**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
+require plugin_dir_path( __FILE__ ) . 'includes/class-miiie-module-import.php';
 
 /**
  * Begins execution of the plugin.
@@ -73,10 +73,10 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
  *
  * @since    1.0.0
  */
-function run_plugin_name() {
+function run_miiie_module_import() {
 
-	$plugin = new Plugin_Name();
+	$plugin = new MIIIE_Module_Import();
 	$plugin->run();
 
 }
-run_plugin_name();
+run_miiie-module-import();
